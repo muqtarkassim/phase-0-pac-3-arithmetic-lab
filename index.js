11869c1c4745952bb4251f8e7ad03dcf214219ad
muqtar <muqtarabdiaziz8@gmail.com>
2023-09-12 15:17:06 +0300

    completed

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+return a+b;
+}
+function subtract(a,b){
+
+return a-b;
+}
+function multiply(a,b){
+    return a*b;
+    }
+    function divide(a,b){
+        return a/b;
+        }
+
+
+        function makeInt(n){
+            const Pg=parseInt(n, 10);
+            return Pg;
+        }
+       function preserveDecimal(n){
+        const Dec= parseFloat(n);
+        return( Dec
+            )
+       }
+       function increment(n) {
+        n=n+1;
+        return n
+       }
+       function decrement(n) {
+        n=n-1;
+        return n
+       }
